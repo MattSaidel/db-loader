3472f483cb1c464f05300381ae00493e6c368eb6
Matthew Saidel <matthewsaidel@Matthews-MacBook-Pro.local>
2014-01-02 13:40:43 -0500

    edit bootstap

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,7 @@
 import sys, os
+import argparse
+from sqlalchemy import Column, String, MetaData, Table
+import csv
 
 sys.path.insert(0, os.path.dirname('/Users/matthewsaidel/Code/db-loader/*'))
 #sys.path.insert(0, os.path.dirname('db-loader/etl/'))
@@ -9,5 +12,16 @@ sys.path.insert(0, os.path.dirname('/Users/matthewsaidel/Code/db-loader/*'))
 #sys.path.insert(0, os.path.dirname(os.getcwd()))
 print sys.path
 
+
 from datasource import engines
-from etl import fec_dataloader
+from etl import fec_dataloader as dl
+
+dl.load_csv_into_memory(file_input) 
+dl.create_fec_master(engine)
+dl.load_to_db(data, table, engine)
+dl.main(args)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+
