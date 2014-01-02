8e2351a8e31043a31daa28b798b767af7f1fa2c2
Matthew Saidel <matthewsaidel@Matthews-MacBook-Pro.local>
2014-01-02 00:22:56 -0500

    small edits to bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,8 @@
 import sys, os
 
-sys.path.insert(0, os.path.dirname('__db-loader__'))
+sys.path.insert(0, os.path.dirname('/home/matthew/Code/db-loader'))
+
+#alternatively
+
+#sys.path.insert(0, os.path.dirname(os.getcwd())
+
