5e522f64be2a165dc44b306d628e33d0f06eb613
Matthew Saidel <matthewsaidel@Matthews-MacBook-Pro.local>
2014-01-02 12:20:43 -0500

    attempting to fix bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,11 @@
 import sys, os
 
-#sys.path.insert(0, os.path.dirname('db-loader'))
+sys.path.insert(0, os.path.dirname('/Users/matthewsaidel/Code/db-loader/*'))
+#sys.path.insert(0, os.path.dirname('db-loader/etl/'))
+#sys.path.insert(0, os.path.dirname('db-loader/datasource/'))
 
 #alternatively
 
-sys.path.insert(0, os.path.dirname(os.getcwd()))
+#sys.path.insert(0, os.path.dirname(os.getcwd()))
+print sys.path
 
